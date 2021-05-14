ffb6f6c4ac0f115f2b635b0e17f481bea7f112be
ben@globalmechanic.com <ben@globalmechanic.com>
2021-05-14 11:15:27 -0700

    previous overridden of min/max from js math module moved to array module
    makes more sense there, I think, as the overridden functionality breaks existing  Math.min/Math.max functionality

---
+++ packages/array/src/index.ts
@@ -0,0 +1,51 @@
+import adjacent from './adjacent'
+import shuffle from './shuffle'
+import unique from './unique'
+import pluck from './pluck'
+import flatten from './flatten'
+import random from './random'
+import ensure from './ensure'
+import remove from './remove'
+
+import isArrayLike from './is-array-like'
+import isIterable from './is-iterable'
+
+import { min, max } from './min-max'
+import wrap from './wrap'
+import unwrap from './unwrap'
+import first from './first'
+import last from './last'
+
+import SortedArray, { ascending, descending } from './sorted-array'
+import type { Sortable } from './sorted-array'
+
+/*** Public Exports ***/
+
+export {
+
+    adjacent,
+    shuffle,
+    unique,
+    pluck,
+    flatten,
+    random,
+
+    ensure,
+    remove,
+
+    first,
+    last,
+    min,
+    max,
+    wrap,
+    unwrap,
+
+    isArrayLike,
+    isIterable,
+
+    SortedArray,
+    Sortable,
+    ascending,
+    descending,
+
+}
