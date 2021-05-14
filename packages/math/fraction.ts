c30ea5363dd5cb7253c6aeaa07731592c20ed259
ben@globalmechanic.com <ben@globalmechanic.com>
2021-05-14 03:24:00 -0700

    started math module

---
+++ packages/math/fraction.ts
@@ -0,0 +1,51 @@
+/*** Types ***/
+
+type Fraction = readonly [numerator: number, denominator: number]
+
+/*** Shortcuts ***/
+
+const { pow, floor } = Math
+
+/*** Helper ***/
+
+function getNumberOfDecimalPlaces(value: number): number {
+    return floor(value) === value
+        ? 0
+        : value
+            .toString()
+            .split('.')[1]
+            .length
+}
+
+function getGreatestCommonDivisor(numerator: number, denominator: number): number {
+
+    return denominator > Number.EPSILON
+        ? getGreatestCommonDivisor(denominator, numerator % denominator)
+        : numerator
+
+}
+
+/*** Main ***/
+
+function toFraction(value: number): Fraction {
+
+    if (Number.isNaN(value))
+        throw new Error('cannot convert NaN to a fraction')
+
+    const denominator = pow(10, getNumberOfDecimalPlaces(value))
+    const numerator = value * denominator
+
+    const divisor = getGreatestCommonDivisor(numerator, denominator)
+
+    return [
+        numerator / divisor,
+        denominator / divisor
+    ]
+
+}
+
+/*** Exports ***/
+
+export default toFraction
+
+export { Fraction }
