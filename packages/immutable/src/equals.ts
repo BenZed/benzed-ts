ce47fd6624614524720c6c6fbb4e0723e448ea3d
ben@globalmechanic.com <ben@globalmechanic.com>
2021-05-16 17:46:19 -0700

    finished equals implementation

---
+++ packages/immutable/src/equals.ts
@@ -0,0 +1,170 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-this-alias */
+
+import { $$equals, getKeys, isReferable, Prototypal } from './util'
+import { isIterable, isArrayLike } from '@benzed/array'
+
+/*** Types ***/
+
+interface Comparable<T> {
+    [$$equals]: (this: Readonly<T>, right: unknown) => boolean
+}
+
+function isComparable<T>(input: unknown): input is Comparable<T> {
+    return input != null &&
+        typeof (input as Comparable<T>)[$$equals] === 'function'
+}
+
+/*** Helper ***/
+
+function compareWithImplementation<T>(left: T, right: unknown): boolean {
+
+    if (left == null || right == null || Object.is(left, right))
+        return Object.is(left, right)
+
+    const isLeftComparable = isComparable(left)
+    if (isLeftComparable && (left as any)[$$equals](right))
+        return true
+
+    if (isComparable(right)) {
+        const hasDifferingImplementation =
+            !isLeftComparable ||
+            (left as any)[$$equals] !== right[$$equals]
+
+        if (hasDifferingImplementation && right[$$equals](left))
+            return true
+    }
+
+    return false
+}
+
+function compareArrayLikes<T>(left: ArrayLike<T>, right: ArrayLike<T>): boolean {
+
+    if (left.length !== right.length)
+        return false
+
+    for (let i = 0; i < left.length; i++) {
+        if (!compareWithImplementation(left[i], right[i]))
+            return false
+    }
+
+    return true
+}
+
+/*** Standard Implementations ***/
+
+function equalIs<T>(this: T, right: unknown): boolean {
+    const left = this
+    return Object.is(left, right)
+}
+
+function equalIterable<T extends Iterable<U>, U>(this: Readonly<T>, right: unknown): boolean {
+    const left = this
+
+    if (!isIterable(right))
+        return false
+
+    return compareArrayLikes(
+        [...left as T],
+        [...right]
+    )
+}
+
+function equalArrayLike<T>(this: readonly T[], right: unknown): boolean {
+    const left = this
+
+    if (!isArrayLike(right))
+        return false
+
+    return compareArrayLikes(left, right)
+}
+
+function equalObject<T>(this: T, right: unknown): boolean {
+
+    if (typeof right !== 'object' || right === null)
+        return false
+
+    const left = this
+
+    const leftKeys = getKeys(left)
+    const rightKeys = getKeys(right)
+
+    if (leftKeys.length !== rightKeys.length)
+        return false
+
+    for (const key of leftKeys) {
+        if (!compareWithImplementation(left[key], (right as any)[key]))
+            return false
+    }
+
+    return true
+}
+
+function equalDate(this: Readonly<Date>, right: unknown): boolean {
+    const left = this
+
+    return isReferable(right) &&
+        typeof right.getTime === 'function' &&
+        left.getTime() === right.getTime()
+}
+
+function equalRegExp(this: Readonly<RegExp>, right: unknown): boolean {
+    const left = this
+
+    return right instanceof RegExp && left.toString() === right.toString()
+}
+
+/*** Add Standard Implementations ***/
+
+{
+
+    const addToPrototype = <T>(
+        { prototype }: Readonly<Prototypal>,
+        implementation: Comparable<T>[typeof $$equals]
+    ): void => {
+
+        Object.defineProperty(prototype, $$equals, {
+            value: implementation,
+            writable: true
+        })
+    }
+
+    for (const Immutable of [String, Boolean, Number, Symbol, Function])
+        addToPrototype(Immutable, equalIs)
+
+    for (const Iterable of [Map, Set])
+        addToPrototype(Iterable, equalIterable)
+
+    addToPrototype(Object, equalObject)
+    addToPrototype(RegExp, equalRegExp)
+    addToPrototype(Date, equalDate)
+
+    for (const ArrayType of [
+        Array,
+        Int8Array,
+        Uint8Array,
+        Uint8ClampedArray,
+        Int16Array,
+        Uint16Array,
+        Int32Array,
+        Uint32Array,
+        Float32Array,
+        Float64Array
+    ])
+        addToPrototype<any[]>(ArrayType, equalArrayLike)
+}
+
+/*** Main ***/
+
+function equals<T>(a: T, b: unknown): boolean {
+    return compareWithImplementation(a, b)
+}
+
+/*** Exports ***/
+
+export default equals
+
+export {
+    equals,
+    Comparable,
+    isComparable
+}
