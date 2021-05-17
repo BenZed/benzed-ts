f3a5abe659dd2673fbd0ca34e409efeef87b42a2
ben@globalmechanic.com <ben@globalmechanic.com>
2021-05-16 17:46:37 -0700

    started immutable array methods

---
+++ packages/immutable/src/array/index-of.test.ts
@@ -0,0 +1,27 @@
+import indexOf from './index-of'
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+describe('value-equal indexOf()', () => {
+
+    it('returns the index of a primitive in an array', () => {
+        const arr = [0, 1, 2, 3, 4, 5, 6]
+        expect(indexOf(arr, 6)).toEqual(6)
+    })
+
+    it('returns the index of a value-equal object in an array', () => {
+        const arr = [{ foo: 'bar' }, { cake: 'town' }]
+        expect(indexOf(arr, { cake: 'town' })).toEqual(1)
+    })
+
+    it('works on array-likes', () => {
+        const arrlike = {
+            length: 2,
+            0: { foo: 'bar' },
+            1: { cake: 'town' },
+            2: { you: 'suck, jimmy' }
+        }
+        expect(indexOf(arrlike, { cake: 'town' })).toEqual(1)
+    })
+
+})
---
+++ packages/immutable/src/array/index-of.ts
@@ -0,0 +1,25 @@
+import equals from '../equals'
+
+/**
+ * Returns the value-equal index of a given value in a given array-like.
+ * 
+ * ```typescript 
+ * 
+ * indexOf(
+ *   [{ amount: 10 }, { amount: 20}, { amount: 30 }], 
+ *   { amount: 20 }
+ * ) // 1
+ * 
+ * ```
+ * @param value 
+ * @returns Numeric index, or -1 if none could be found.
+ */
+export default function indexOf<T>(arrayLike: ArrayLike<T>, value: T): number {
+
+    for (let i = 0; i < arrayLike.length; i++) {
+        if (equals(arrayLike[i], value))
+            return i
+    }
+
+    return -1
+}
