f573a291ef2bff1b2933a9875fb9d37118e04720
ben@globalmechanic.com <ben@globalmechanic.com>
2021-05-21 01:33:20 -0700

    started schema package

---
+++ packages/schema/src/jsx.d.ts
@@ -0,0 +1,20 @@
+/* eslint-disable 
+    @typescript-eslint/no-namespace,
+    @typescript-eslint/no-explicit-any
+*/
+
+import { DefaultValidators, Validator } from './schema'
+
+/*** JSX ***/
+
+declare global {
+
+    namespace JSX {
+
+        type IntrinsicElements = DefaultValidators
+
+        type Element = Validator<any>
+
+    }
+
+}
