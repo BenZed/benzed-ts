06965b870fd80c9c781042ad94fdf6691ac87a42
ben@globalmechanic.com <ben@globalmechanic.com>
2021-09-20 18:44:56 -0700

    removed sidebar provider
    too hacky/gimmicky

---
+++ packages/operandi/src/state/state.ts
@@ -0,0 +1,17 @@
+import { V2String, V2 } from '@benzed/math'
+
+import { Frame } from './frame'
+
+/*** Types ***/
+
+interface State {
+    frames: Record<V2String, Frame>
+}
+
+/*** Interface ***/
+
+/*** Exports ***/
+
+export {
+    State
+}
