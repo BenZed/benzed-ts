bc8b769f0387dfae1b6ec8abf235eed13bf27ada
ben@globalmechanic.com <ben@globalmechanic.com>
2022-01-14 20:31:14 -0800

    util exports update

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -4,3 +4,16 @@ export * from './this-shift'
 export * from './reduce-to-void'
 export * from './assert-defined'
 export * from './promisify'
+
+export {
+    createLogger,
+    Logger,
+    LoggerOptions,
+    LogHandler
+} from './logger'
+
+export {
+    ansi,
+    AnsiColor,
+    AnsiOptions
+} from './ansi'
