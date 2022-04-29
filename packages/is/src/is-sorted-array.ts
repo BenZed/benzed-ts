eb79e20f057a11c62180027a4ae6ea818dcecc96
ben@globalmechanic.com <ben@globalmechanic.com>
2022-04-29 12:00:38 -0700

    publish

--- packages/is/src/is-sorted-array.ts
+++ packages/is/src/is-sorted-array.ts
@@ -3,22 +3,31 @@ import { Sortable } from './types'
 
 /*** Main ***/
 
+/**
+ * Returns true if the given value is a sorted array.
+ * @param input Any value.
+ * @param isAscending True if array must be ascending, false if must be descending, 
+ * or undefined for irrelevent.
+ * @returns true if sorted, false otherwise.
+ */
 function isSortedArray<T extends Sortable>(
-    input: unknown
+    input: unknown,
+    isAscending?: boolean
 ): input is T[] {
 
     if (!isArrayOfSortable(input))
         return false
 
-    const isAscending = input[0] < input[input.length - 1]
+    if (isAscending === undefined)
+        isAscending = input[0] < input[input.length - 1]
 
     let value: Sortable = isAscending ? -Infinity : Infinity
 
     for (const item of input) {
 
         const isInOrder = isAscending
-            ? item > value
-            : item < value
+            ? item >= value
+            : item <= value
 
         if (isInOrder)
             value = item
