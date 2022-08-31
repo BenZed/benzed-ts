12c89e3f9f125fcc75db3ab19f1d56c9eeac813c
BenZed <bengaumond@gmail.com>
2022-08-31 09:56:17 -0700

    added some utility types

--- packages/util/src/types.ts
+++ packages/util/src/types.ts
@@ -1,29 +1,78 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable 
+    @typescript-eslint/no-explicit-any 
+*/
 
-export type Func<A extends any[] = unknown[], V = unknown> = (...args: A) => V
+export type Func<A extends any[] = unknown[], V = unknown, T = void> = (this: T, ...args: A) => V
 
 export type Json =
     null | string | number | boolean |
     Json[] |
     { [prop: string]: Json }
 
+/**
+ * Reduce two types to only their matching key values.
+ */
+export type Collapse<LEFT, RIGHT> =
+    {
+        [K in keyof LEFT as
+
+        // Only include key of left if right has the same key and value
+        /**/ K extends keyof RIGHT ?
+            /**/ RIGHT[K] extends LEFT[K]
+                /**/ ? K
+                /**/ : never
+            /**/ : never
+
+        ]: LEFT[K]
+    }
+
+/**
+ * Create an interesection out of an arbitrary number of types
+ */
+export type Intersect<T extends any[]> = T extends [infer FIRST, ...infer REST]
+    ? FIRST & Intersect<REST>
+    : unknown
+
+/**
+ * Merge an arbitrary number of types into one. 
+ */
+export type Merge<T extends any[]> =
+    {
+        [K in keyof Intersect<T>]: Intersect<T>[K]
+    }
+
+/**
+ * 
+ */
 export type RequirePartial<T, K extends keyof T> =
-    {
-        [TK in keyof T as TK extends K ? TK : never]-?: T[TK]
-    } & {
-        [TK in keyof T as TK extends K ? never : TK]: T[TK]
-    }
+    Merge<[
+        {
+            [TK in keyof T as TK extends K ? TK : never]-?: T[TK]
+        },
+        {
+            [TK in keyof T as TK extends K ? never : TK]: T[TK]
+        }
+    ]>
 
-export type UndefinedToOptional<T> =
-    {
-        [K in keyof T as undefined extends T[K] ? never : K]-?: T[K]
-    } &
-    {
-        [K in keyof T as undefined extends T[K] ? K : never]?: T[K]
-    }
+/**
+ * Get a type where the given values are optional
+ */
+export type Optional<T, V> =
+    Merge<[
+        {
+            [K in keyof T as V extends T[K] ? never : K]-?: T[K]
+        },
+        {
+            [K in keyof T as V extends T[K] ? K : never]?: T[K]
+        }
+    ]>
 
-export type TypeMatchedKeys<T1, K1 extends keyof T1, T2> = {
-    [K2 in keyof T2]: T2[K2] extends T1[K1] ? K2 : never
-}[keyof T2]
+/**
+ * Get a type where any undefined property is optional
+ */
+export type UndefinedToOptional<T> = Optional<T, undefined>
 
+/**
+ * Get the string keys of a type.
+ */
 export type StringKeys<T> = Extract<keyof T, string>
