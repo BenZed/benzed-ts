8a2fe9bfa6be9eae04d2c458425663cfabeb7a2c
BenZed <bengaumond@gmail.com>
2022-10-03 18:20:48 -0700

    pagination object added to schema

--- packages/feathers/src/schemas/util.ts
+++ packages/feathers/src/schemas/util.ts
@@ -5,9 +5,17 @@ import { $ } from '@benzed/schema'
 /**
  * Number in port range, 1024 - 65535
  */
-export const $port = $.number().range(1024, 65535)
+export const $port = $.integer().range(1024, 65535)
 
 /**
  * TODO make this an object id
  */
 export const $id = $.string()
+
+/**
+ * Pagination object.
+ */
+export const $pagination = $({
+    default: $.integer(),
+    max: $.integer()
+})
