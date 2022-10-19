7971aeed47b032c196c7ac007b21932b54b482eb
BenZed <bengaumond@gmail.com>
2022-10-19 16:10:59 -0700

    better type signature on reduce-to-void

--- packages/util/src/reduce-to-void.ts
+++ packages/util/src/reduce-to-void.ts
@@ -1,16 +1,26 @@
 // import { isPromise } from '@benzed/is'
 
+/*** Helper ***/
+
 const isPromise = (i: unknown): i is Promise<unknown> => i instanceof Promise
+
+/*** Types ***/
+
+type ReduceToVoid<T> = (T extends Promise<unknown> | Promise<unknown>[] ? Promise<void> : void)
 
 /*** reduceToVoid ***/
 
-function reduceToVoid(input?: unknown): void | Promise<void> {
+function reduceToVoid<T>(
+    input?: T
+): ReduceToVoid<T> {
 
     if (isPromise(input))
-        return input.then(reduceToVoid)
+        return input.then(reduceToVoid) as ReduceToVoid<T>
 
     if (Array.isArray(input) && input.some(isPromise))
-        return Promise.all(input).then(reduceToVoid)
+        return Promise.all(input).then(reduceToVoid) as ReduceToVoid<T>
+
+    return undefined as ReduceToVoid<T>
 }
 
 /*** Exports ***/
