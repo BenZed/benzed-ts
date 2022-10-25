5c3806e0560ab3bc719eee8b42439daba26a3742
BenZed <bengaumond@gmail.com>
2022-10-24 18:59:33 -0700

    organized

--- packages/feathers/src/builder/builder.test.ts
+++ packages/feathers/src/builder/builder.test.ts
@@ -3,16 +3,38 @@ import { builder } from './builder'
 import { Application } from '@feathersjs/feathers'
 
 import { expectTypeOf } from 'expect-type'
+import BuildComponent from './build-component'
+import { Empty } from '@benzed/util/lib'
 
 /*** Setup ***/
 
+class TestBuildComponent extends BuildComponent<Empty, [], false> {
+
+    single = false as const
+
+    required = []
+
+    protected _createBuildEffect(required: []): Empty {
+        return {}
+    }
+
+}
+
 /*** Tests ***/
 
-it.skip(`creates feathers applications`, () => {
+it(`creates feathers applications`, () => {
     
-    const app = builder.build()
+    const app = builder
+        .add(new TestBuildComponent())
+        .build()
 
     expectTypeOf<typeof app>().toMatchTypeOf<Application>()
+})
+
+it(`throws if no components have been added`, () => {
+
+    expect(() => builder.build())
+
 })
 
 it.todo(`add() must use build components`)
--- packages/feathers/src/builder/builder.ts
+++ packages/feathers/src/builder/builder.ts
@@ -1,26 +1,21 @@
-
-import { feathers } from '@feathersjs/feathers'
-
 import BuildComponent, { 
-
     App, 
-
     BuildComponents, 
     BuildContext,
-
     FromBuildEffect 
-
 } from './build-component'
 
 import { getDefaultConfiguration } from '../util'
 
 import { 
-    Compile, 
-    StringKeys, 
-    through 
+    Merge, 
+    Empty, 
+    StringKeys
 } from '@benzed/util'
 
 import { Node } from '@benzed/ecs'
+
+import { feathers } from '@feathersjs/feathers'
 
 /*** Eslint ***/
 
@@ -30,22 +25,26 @@ import { Node } from '@benzed/ecs'
 
 /*** Types ***/
 
-type BuiltConfig<C extends BuildComponents> = FromBuildEffect<C>['config']
-
 type BuiltServices<C extends BuildComponents> = FromBuildEffect<C>['services']
+type BuiltConfig<C extends BuildComponents> = FromBuildEffect<C>['config']
+type BuildExtensions<C extends BuildComponents> = FromBuildEffect<C>['extend']
 
-export type BuiltApplication<C extends BuildComponents> = 
-    App<BuiltServices<C>, BuiltConfig<C>>
+type BuiltApplication<C extends BuildComponents> = 
+    BuildExtensions<C> extends Empty 
+        ? App<BuiltServices<C>, BuiltConfig<C>>
+        : App<BuiltServices<C>, BuiltConfig<C>> & BuildExtensions<C>
 
-type BuilderInput<C extends BuildComponents> = BuiltConfig<C> 
+type BuilderInput<C extends BuildComponents> = BuiltConfig<C>
 
-type BuilderOutput<C extends BuildComponents> =     
-    Compile<BuiltApplication<C>, App, false>
+type BuilderOutput<C extends BuildComponents> = BuiltApplication<C>
 
 type AddBuildComponent<C extends BuildComponents> = BuildComponent<any,any,any>
 
 /*** Builder ***/
 
+/**
+ * ECS Node for creating feathers applications
+ */
 class Builder<C extends BuildComponents> extends Node<BuilderInput<C>, BuilderOutput<C>, C> {
 
     add<Cx extends AddBuildComponent<C>>(
@@ -63,7 +62,7 @@ class Builder<C extends BuildComponents> extends Node<BuilderInput<C>, BuilderOu
         const ctx = this._computeBuildContext()
 
         const app = this._createApplication(config, ctx.config)
-        return app
+        return app as BuilderOutput<C>
     }
 
     build(config: BuilderInput<C> = getDefaultConfiguration()): BuilderOutput<C> {
@@ -114,6 +113,6 @@ class Builder<C extends BuildComponents> extends Node<BuilderInput<C>, BuilderOu
 /**
  * An ECS for building feathers applications
  */
-export const builder = new Builder<[]>([{ compute: through }] as unknown as any)
+export const builder = new Builder<[]>([])
 
 export default builder
