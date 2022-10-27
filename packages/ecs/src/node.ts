4f530afaba77246c7c607066503c262ffac862b3
BenZed <bengaumond@gmail.com>
2022-10-26 18:13:30 -0700

    extended validation

--- packages/ecs/src/node.ts
+++ packages/ecs/src/node.ts
@@ -14,47 +14,46 @@ import { Component } from './component'
  */
 export type Components<I = unknown, O = I> = readonly Component<I,O>[]
 
-type GetComponent<C extends Components, Cx extends new () => C[number]> = 
+type GetComponent<C extends Components, Cx extends ComponentConstructor<any, A>, A extends boolean> = 
     C extends [infer I, ...infer Ir] 
         ? I extends InstanceType<Cx> 
             ? I
             : Ir extends Components 
-                ? GetComponent<Ir, Cx> 
+                ? GetComponent<Ir, Cx, A> 
                 : never
         : never
 
-type HasComponent<C extends Components, Cx extends new () => Component<any>> = 
+type HasComponent<C extends Components, Cx extends ComponentConstructor<any, A>, A extends boolean> = 
     InstanceType<Cx> extends C[number] 
         ? true 
         : false
 
 /*** Node ***/
 
+type ComponentConstructor<C extends Components, A extends boolean = false> = 
+    A extends true 
+        ? abstract new (...args: any[]) => C[number]  
+        : new (...args: any[]) => C[number]
+
 /**
  * Node is the base class for any component that is comprised of other components.
  * 
  * By convention, Nodes are created with a builder pattern; 
  * each extended class should have a static .create() method and 
- * a private constructor. 
- * 
- * This is to enforce type validation on components 
+ * a private constructor which would be used to enforce type
+ * validation on private components
  */
 export abstract class Node<I, O, C extends Components> extends Component<I,O> {
 
     constructor(
         readonly components: C
     ) {
-
-        if (components.length === 0)
-            throw new Error('Node must be created with at least one component')
-
         super() 
+        this._validateComponents()
     }
 
-    abstract add(...args: [unknown] | [unknown, unknown]): unknown
-
     has<Ix extends number>(index: Ix): Ix extends IndexesOf<C> ? true : false 
-    has<T extends new () => Component>(constructor: T): HasComponent<C, T>
+    has<T extends ComponentConstructor<C, A>, A extends boolean>(type: T): HasComponent<C, T, A>
     has(input: unknown): unknown {
         return is.number(input) 
             ? input in this.components
@@ -62,7 +61,7 @@ export abstract class Node<I, O, C extends Components> extends Component<I,O> {
     }
 
     get<Ix extends IndexesOf<C>>(index: Ix): C[Ix] 
-    get<T extends new () => C[number]>(constructor: T): GetComponent<C, T>
+    get<T extends ComponentConstructor<C, A>, A extends boolean>(type: T): GetComponent<C, T, A>
     get(input: unknown): unknown {
 
         const component = is.number(input) 
@@ -71,7 +70,7 @@ export abstract class Node<I, O, C extends Components> extends Component<I,O> {
     
         if (!component) {
             throw new Error(
-                'Could not find component ' + (
+                `Could not find component ` + (
                     is.number(input) 
                         ? `at index ${input}` 
                         : `of type ${(input as any).name}`
@@ -88,6 +87,33 @@ export abstract class Node<I, O, C extends Components> extends Component<I,O> {
 
     get last(): Last<C> {
         return this.components.at(-1) as Last<C>
+    }
+
+    /**
+     * No validation by default, extend this method
+     */
+    protected _validateComponents(): void { /**/ }
+
+    protected _assertAtLeastOneComponent(): void {
+        if (this.components.length === 0)
+            throw new Error(`Node must be created with at least one component`)
+    }
+
+    protected _assertConflicting<A extends boolean>(...types: ComponentConstructor<C, A>[]): void {
+        const found = types.filter(t => this.has(t))
+        if (found.length > 0)
+            throw new Error(`${this.constructor.name} cannot be used with conflicting components: ${found.map(m => m.name)}`)
+    }
+
+    protected _assertRequired<A extends boolean>(...types: ComponentConstructor<C, A>[]): void {
+        const missing = types.filter(t => !this.has(t))
+        if (missing.length > 0)
+            throw new Error(`${this.constructor.name} missing required components: ${missing.map(m => m.name)}`)
+    }
+
+    protected _assertSingle(): void {
+        if (this.has(this.constructor as ComponentConstructor<C>))
+            throw new Error(`${this.constructor.name} cannot be used more than once.`)
     }
 
 }
