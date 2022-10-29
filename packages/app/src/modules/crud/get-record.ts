0e064e554a2a0ba8ae04c914a371dcd9b6d53ce9
BenZed <bengaumond@gmail.com>
2022-10-29 12:55:06 -0700

    started crud command modules, but I want to button down commands first

---
+++ packages/app/src/modules/crud/get-record.ts
@@ -0,0 +1,50 @@
+import $, { Infer } from "@benzed/schema"
+import { Command } from "../command"
+import { CommandModule } from "../module"
+import { $logIcon } from "../schemas"
+
+/*** Types ***/
+
+interface GetCommand<N extends string> extends Command {
+
+    name: `get-${N}`
+
+}
+
+interface GetSettings<N extends string> extends _GetSettings {
+
+    collection: N
+
+}
+
+type _GetSettings = Infer<typeof $getSetting>
+const $getSetting = $({
+    logIcon: $logIcon.default(`🗄️`),
+    collection: $.string.asserts(name => name.startsWith(`get-`), `must start with "get-"`)
+})
+
+/*** Main ***/
+
+class Get<N extends string> extends CommandModule<GetCommand<N>, { collection: N }> {
+
+    static create<Nx extends string>(settings: GetSettings<Nx>): Get<Nx> {
+        return new Get($getSetting.validate(settings)) as Get<Nx>
+    }
+
+    protected _execute(command: GetCommand<N>): object | Promise<object> {
+        return {}
+    }
+
+    canExecute(command: Command): command is GetCommand<N> {
+        return command.name === `get-${this.settings.collection}`
+    }
+
+}
+
+/*** Exports ***/
+
+export default Get 
+
+export {
+    Get
+}
