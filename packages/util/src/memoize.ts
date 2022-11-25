d3ba98f95d8b1ed929caddb0669b612c5f9e1c70
BenZed <bengaumond@gmail.com>
2022-11-25 15:39:31 -0800

    fixed memoization

--- packages/util/src/memoize.ts
+++ packages/util/src/memoize.ts
@@ -1,26 +1,96 @@
 import { defineName } from './property'
-import { ValuesMap } from './structures'
+import { ValueMap, ValuesMap } from './structures'
+import { Func } from './types'
+
+// Helper 
+
+function trimCacheToSize(
+    cache: ValueMap<unknown[], unknown>,
+    size: number
+): void {
+
+    const keys = cache['_keys']
+    const values = cache['_values']
+
+    if (cache.size > size) {
+        const deleteCount = cache.size - size
+
+        keys.splice(0, deleteCount)
+        values.splice(0, deleteCount)
+    }
+}
+
+function resolveOptions<F extends Func>(
+    func: F, 
+    options?: string | number | MemoizeOptions<F>
+): Required<MemoizeOptions<F>> {
+
+    const { 
+        name = func.name,
+        maxCacheSize = Infinity, 
+        cache = new ValuesMap() as ValuesMap<Parameters<F>, ReturnType<F>>
+
+    } = typeof options === 'string'
+        ? { name: options }
+        : typeof options === 'number'
+            ? { maxCacheSize: options }
+            : options ?? {}
+    return {
+        name,
+        maxCacheSize,
+        cache
+    }
+}
+
+// Type 
+
+export type Memoized<F extends Func> = F
+
+export interface MemoizeOptions<F extends Func> {
+    name?: string
+    maxCacheSize?: number
+    cache?: ValueMap<Parameters<F>, ReturnType<F>>
+}
+
+// Main
+
+export function memoize<F extends Func>(f: F, name?: string): Memoized<F>
+
+export function memoize<F extends Func>(f: F, maxCacheSize?: number): Memoized<F>
+
+export function memoize<F extends Func>(f: F, options?: MemoizeOptions<F>): Memoized<F>
 
 /**
  * get a method that caches it's output based in the identicality of it's arguments
  */
-export function memoize<A extends unknown[], R>(func: (...args: A) => R, name = func.name): (...args: A) => R {
+export function memoize<F extends Func>(
+    func: F, 
+    options?: string | number | MemoizeOptions<F>
+): Memoized<F> {
 
-    const memoized = (...args: A ): R => {
+    // Get Options
+    const { name, cache, maxCacheSize } = resolveOptions(func, options)
 
-        if (memoize.cache.has(args)) 
-            return memoize.cache.get(args) as R
+    function memoized(this: unknown, ...args: Parameters<F> ): ReturnType<F> {
 
-        const value = func(...args as A)
-        if (value instanceof Promise)
-            value.then(result => memoize.cache.set(args, result))
+        // get memoized value
+        if (cache.has(args)) 
+            return cache.get(args) as ReturnType<F>
 
-        memoize.cache.set(args, value)
+        // create memoized value
+        const value = func.apply(this, args)
+        cache.set(args, value)
 
-        return value as R
+        // trim cache
+        if (cache instanceof ValueMap)
+            trimCacheToSize(cache, maxCacheSize)
+
+        return value as ReturnType<F>
     }
 
-    return defineName(memoized, name)
+    return defineName(memoized, name) as Memoized<F>
 }
 
-memoize.cache = new ValuesMap<unknown[],unknown>()
+//// Extend ////
+
+memoize.options = resolveOptions
