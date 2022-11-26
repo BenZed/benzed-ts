d7fd95f9354af9ebb348585f14281abeac54c605
BenZed <bengaumond@gmail.com>
2022-11-26 00:29:24 -0800

    enum work

--- packages/is/src/schemas/enum.ts
+++ packages/is/src/schemas/enum.ts
@@ -1,8 +1,19 @@
+import { Primitive } from '@benzed/util/lib'
 import { schema, Schema } from '../schema'
-import { validator } from '../validator'
+import { validator, Validator } from '../validator'
 
-interface EnumSchema<T> extends Schema<T> {
-    
+//// Symbols ////
+
+const $$enum = Symbol('enum-validator')
+
+//// Types ////
+
+interface EnumSchema<T extends Primitive> extends Schema<T> {
+    options: T[]
+}
+
+interface EnumValidator<T extends readonly Primitive[]> extends Validator<unknown, T[number]> {
+    options: T
 }
 
 const enumValidator = validator({
@@ -11,10 +22,16 @@ const enumValidator = validator({
 
     assert(input: unknown): boolean {
         return this.options.includes(input)
-    }
+    },
 
-})
+}, $$enum)
 
-const enumSchematic = schema({
+const enumSchematic = schema(enumValidator)
 
-})
+//// Exports ////
+
+export function enumSchema<O extends readonly Primitive[]>(...options: O): EnumSchema<O[number]> {
+
+    return enumSchematic.validates({ [$$enum]: true, options }) as EnumSchema<O[number]>
+
+}
