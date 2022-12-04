e11fef1726548d0f5232d6912d701abd9c7c0ec4
BenZed <bengaumond@gmail.com>
2022-12-04 11:21:38 -0800

    Update empty.test.ts

--- packages/util/src/types/empty.test.ts
+++ packages/util/src/types/empty.test.ts
@@ -2,14 +2,11 @@ import { Empty, isEmpty } from './empty'
 import { expectTypeOf } from 'expect-type'
 
 it('isEmpty', () => {
-
     expect(isEmpty({})).toBe(true)
-
 })
 
 it('is not empty', () => {
     expect(isEmpty({ foo: 'bar' })).toBe(false)
-
 })
 
 it('Empty type', () => {
