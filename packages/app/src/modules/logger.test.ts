689f18ad210ca8f36d9449467e1969adfc2b6cba
BenZed <bengaumond@gmail.com>
2022-12-08 14:36:21 -0800

    Logger module finished

---
+++ packages/app/src/modules/logger.test.ts
@@ -0,0 +1,74 @@
+import { toVoid } from '@benzed/util'
+
+import { Module } from '../module'
+import { App } from '../app'
+
+import { Logger } from './logger'
+
+//// Setup ////
+
+const testLogger = Logger.create({
+    onLog: toVoid,
+    cacheLength: 6
+})
+
+class Icon extends Module {
+    static readonly icon = '@' 
+}
+
+class NoIcon extends Module {}
+
+const app = App
+    .create()
+    .useModule(testLogger)
+    .useModule(new Icon())
+    .useModule(new NoIcon())
+
+//// Tests ////
+
+const timestamp = expect.any(String)
+
+let logger: Logger
+let icon: Icon
+let noIcon: NoIcon
+beforeAll(() => app.start())
+
+beforeAll(() => {
+    logger = app.getModule(Logger, true)
+    logger.log`This should be omitted`
+
+    noIcon = app.getModule(NoIcon, true)
+    noIcon.log.info`Info`
+    noIcon.log.warn`Warn`
+    noIcon.log.error`Error`
+    
+    icon = app.getModule(Icon, true)
+    icon.log.info`Info`
+    icon.log.warn`Warn`
+    icon.log.error`Error`
+
+})
+
+afterAll(() => app.stop())
+
+it('takes input of any module\'s log method', () => {
+    expect(logger.logs[0]).toEqual([timestamp, 'Info'])
+})
+
+it('adds warn icon', () => {
+    expect(logger.logs[1]).toEqual([timestamp, '⚠️', 'Warn'])
+})
+
+it('adds error icon', () => {
+    expect(logger.logs[2]).toEqual([timestamp, '‼️', 'Error'])
+})
+
+it('uses module icon, if defined', () => {
+    expect(logger.logs[3]).toEqual([Icon.icon, timestamp, 'Info'])
+    expect(logger.logs[4]).toEqual([Icon.icon, timestamp, '⚠️', 'Warn'])
+    expect(logger.logs[5]).toEqual([Icon.icon, timestamp, '‼️', 'Error'])
+})
+
+it('respects cache length', () => {
+    expect(logger.logs.some(l => l.includes('This should be omitted'))).toBe(false)
+})
--- packages/app/src/modules/logger.ts
+++ packages/app/src/modules/logger.ts
@@ -1,24 +1,53 @@
 
-import { Logger as _Logger, LoggerOptions } from '@benzed/util'
+import { Logger as _Logger, LoggerOptions, PartialOptional } from '@benzed/util'
 import { SettingsModule } from '../module'
+
+////  ////
+
+const DEFAULT_CACHE_LENGTH = 1000
 
 //// Types ////
 
-interface LoggerSettings extends Omit<LoggerOptions, 'header'> {}
+interface LoggerSettings extends Omit<LoggerOptions, 'header'> {
+    
+    /**
+     * Number of logs to keep in the cache.
+     */
+    cacheLength: number
+}
 
 //// Main ////
 
 class Logger extends SettingsModule<LoggerSettings> {
 
-    static create(options: LoggerSettings): Logger {
-        return new Logger(options)
+    static create(options: PartialOptional<LoggerSettings, 'cacheLength' | 'timeStamp'>): Logger {
+        return new Logger({
+            timeStamp: true,
+            cacheLength: DEFAULT_CACHE_LENGTH,
+            ...options
+        })
     }
 
     private constructor(
         options: LoggerSettings
     ) {
         super(options)
-        this._log = _Logger.create(options)
+        this._log = _Logger.create({
+            ...options,
+            onLog: (...args) => {
+
+                this._logs.push(args)
+                while (this._logs.length > this.settings.cacheLength)
+                    this._logs.splice(0, 1)
+                
+                this.settings.onLog(...args)
+            }
+        })
+    }
+
+    private readonly _logs: unknown[][] = []
+    get logs(): readonly unknown[][] {
+        return this._logs
     }
 
     private readonly _log: _Logger
