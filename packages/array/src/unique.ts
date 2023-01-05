285d54ce4b4a6c92d56e91bb3e5f9c13f3f21b4c
BenZed <bengaumond@gmail.com>
2023-01-04 22:02:52 -0800

    commened unique
    for some reason

--- packages/array/src/unique.ts
+++ packages/array/src/unique.ts
@@ -1,3 +1,5 @@
+import { indexesOf } from '@benzed/util'
+
 //// Shortcuts ////
 
 const { splice, findIndex } = Array.prototype
@@ -5,29 +7,52 @@ const { splice, findIndex } = Array.prototype
 //// Main ////
 
 /**
- * Given filter arguements, return true if the provided 
+ * Given filter arguements, return true if the provided value is
+ * unique to the array, false otherwise.
  */
 function unique<T>(value: T, index: number, array: ArrayLike<T>): boolean
+
+/**
+ * Given an arraylike, return an arraylike containing only unique values.
+ * @param arrayLike 
+ */
 function unique<T extends string | ArrayLike<unknown>>(arrayLike: T): T
+
+/**
+ * Bound to an arraylike, return an arraylike containing only unique values.
+ * @param this 
+ */
 function unique<T extends ArrayLike<unknown>>(this: T): T
-function unique(this: unknown, pivot?: unknown, index?: number, array?: ArrayLike<unknown>): unknown {
-    
+function unique(
+    this: unknown, 
+    arrayLikeOrItem?: unknown, 
+    index?: number, 
+    arrayContainingItem?: ArrayLike<unknown>
+): unknown {
+
+    // handle bound signature
     if (this) 
         return unique(this as ArrayLike<unknown>)
     
-    if (array) 
-        return findIndex.call(array, value => Object.is(pivot, value)) === index
-    else   
-        array = pivot as ArrayLike<unknown>
-        
-    if (typeof array === 'string')
-        return unique(array.split('')).join('')
-
-    for (let i = array.length - 1; i >= 0; i--) {
-        if (!unique(array[i], i, array)) 
-            splice.call(array, i, 1)
+    // handle filter signature
+    if (arrayContainingItem) {
+        const item = arrayLikeOrItem
+        return findIndex.call(arrayContainingItem, value => Object.is(item, value)) === index
     }
-    return array
+
+    const arrayLike = arrayLikeOrItem as ArrayLike<unknown>
+
+    // handle string value string
+    if (typeof arrayLike === 'string')
+        return unique(arrayLike.split('')).join('')
+
+    // handle 
+    for (const index of indexesOf(arrayLike, true)) {
+        if (!unique(arrayLike[index], index, arrayLike)) 
+            splice.call(arrayLike, index, 1)
+    }
+
+    return arrayLike
 }
 
 //// Exports ////
