6dbe72780b85cdbb33e2d63eb40f2f49951d0eba
BenZed <bengaumond@gmail.com>
2023-02-15 03:19:32 -0800

    added some module subclasses

--- packages/immutable/src/module/index.ts
+++ packages/immutable/src/module/index.ts
@@ -3,6 +3,8 @@ export * from './find'
 
 export * from './module'
 
+export * from './module-public'
+
 export * from './parent'
 
 export * from './relations'
---
+++ packages/immutable/src/module/module-public.ts
@@ -0,0 +1,67 @@
+import { keysOf } from '@benzed/util'
+import { 
+    AssertModule,
+    Find, 
+    FindFlag, 
+    FindModule, 
+    getParent, 
+    HasModule, 
+    Parent,
+
+    eachAncestor,
+    eachSibling,
+    eachDescendent,
+    getChildren,
+    Module
+} from '.'
+
+//// Main ////
+
+export class ModuleFind extends Module {
+
+    get find(): FindModule {
+        return new Find(this)
+    }
+
+    get has(): HasModule {
+        return new Find(this, FindFlag.Has)
+    }
+    
+    get assert(): AssertModule {
+        return new Find(this, FindFlag.Assert)
+    }
+
+}
+
+/**
+ * A node with public getters for common node relations
+ */
+export class ModulePublic extends ModuleFind {
+
+    // Relations
+
+    get ancestors(): Module[] {
+        return Array.from(eachAncestor(this))
+    }
+    
+    get parent(): Parent<this> {
+        return getParent(this)
+    }
+
+    get siblings(): Module[] {
+        return Array.from(eachSibling(this))
+    }
+
+    get children(): Module[] {
+        const children = getChildren(this)
+        return Array.from({
+            ...children,
+            length: keysOf.count(children)
+        })
+    }
+
+    get descendents(): Module[] {
+        return Array.from(eachDescendent(this))
+    }
+
+}
