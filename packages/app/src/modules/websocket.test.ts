3425cf9e5592b0bbc3f49efa3e729ec2174548a9
BenZed <bengaumond@gmail.com>
2023-03-03 20:01:31 -0800

    scaffolded websocket component

---
+++ packages/app/src/modules/websocket.test.ts
@@ -0,0 +1,13 @@
+import { Websocket } from './websocket'
+
+import { test, it, expect, describe } from '@jest/globals'
+
+//// Tests ////
+
+describe('OnValidate', () => {
+
+    test.todo('asserts unique; no other websocket module can exist in the tree')
+
+    test.todo('asserts root parent; must be parented to the root')
+
+})
