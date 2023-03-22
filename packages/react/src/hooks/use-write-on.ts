b8d72a2d79b89cffe02738d2a7cd8d5775fdeca4
BenZed <bengaumond@gmail.com>
2023-03-22 14:09:13 -0700

    added write-on options

--- packages/react/src/hooks/use-write-on.ts
+++ packages/react/src/hooks/use-write-on.ts
@@ -3,35 +3,69 @@ import { each } from '@benzed/util'
 
 import { useEffect, useState } from 'react'
 
+//// Types ////
+
+interface WriteOnOptions {
+
+    /*
+     * Milliseconds between write update
+     */
+    readonly interval?: number
+
+    /**
+     * Number of characters changed per update. 
+     */
+    readonly changeRate?: number
+
+    /**
+     * Defaults to the target value
+     */
+    readonly initialValue?: string
+}
+
 //// Main ////
 
-const useWriteOn = (target: string, interval = 50) => {
+const useWriteOn = (target: string, options: WriteOnOptions = {}) => {
 
-    const [value, setValue] = useState(target)
+    const {
+        changeRate = 1,
+        initialValue = target,
+        interval = 50 
+    } = options
+
+    const [ value, setValue ] = useState(initialValue)
 
     useEffect(() => onInterval(() => {
 
-        // Match chars
-        for (const index of each.indexOf(target))
-            if (value.charAt(index) !== target.charAt(index)) {
-                const replacedChar = value.slice(0, index) + target.charAt(index) + value.slice(index + 1)
-                setValue(replacedChar)
-                return
+        let changes = changeRate
+        let newValue = value
+
+        change: while (changes-- > 0) {
+            // Match chars
+            for (const index of each.indexOf(target))
+                if (newValue.charAt(index) !== target.charAt(index)) {
+                    const replaceOneChar = newValue.slice(0, index) + target.charAt(index) + newValue.slice(index + 1)
+                    newValue = replaceOneChar
+                    continue change
+                }
+
+            // Increase length
+            if (newValue.length > target.length) {
+                const minusOneChar = newValue.slice(0, -1)
+                newValue = minusOneChar
             }
 
-        // Increase length
-        if (value.length > target.length) {
-            const minusOneChar = value.slice(0, -1)
-            setValue(minusOneChar)
+            // Reduce length
+            else if (newValue.length < target.length) {
+                const plusOneChar = newValue + target.slice(newValue.length, newValue.length + 1)
+                newValue = plusOneChar
+            }
         }
 
-        // Reduce length
-        else if (value.length < target.length) {
-            const plusOneChar = value + target.slice(value.length, value.length + 1)
-            setValue(plusOneChar)
-        }
+        if (newValue !== value) 
+            setValue(newValue)
 
-    }, interval), [value, target, interval])
+    }, interval), [value, target, interval, changeRate])
 
     return value
 }
@@ -41,5 +75,6 @@ const useWriteOn = (target: string, interval = 50) => {
 export default useWriteOn
 
 export {
-    useWriteOn
+    useWriteOn,
+    WriteOnOptions
 }
