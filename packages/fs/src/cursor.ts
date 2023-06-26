f81573e77c9fe26430c8ef24880640ebe50a5102
bengaumond <ben.gaumond@edynamiclearning.com>
2023-06-25 18:04:22 -0700

    minor scaffolding

---
+++ packages/fs/src/cursor.ts
@@ -0,0 +1,21 @@
+
+//// Main ////
+
+/**
+ * A cursor represents a location on the file system, with functionality for
+ * interacting with file content, iterating through directories or 
+ * getting cursors to other locations on the file system.
+ */
+class Cursor {
+
+    constructor(path: string) {
+        //
+    }
+
+}
+
+//// Exports ////
+
+export {
+    Cursor
+}
--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -1,3 +1,10 @@
-export * as default from './export'
+import { Cursor } from './cursor';
 
-export * from './export'
+export * from './cursor'
+
+/**
+ * Create a cursor to a location on the file system.
+ */
+export default async function (path: string): Promise<Cursor> {
+    return new Cursor(path)
+}
